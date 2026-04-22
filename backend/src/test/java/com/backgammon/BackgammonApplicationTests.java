d4fa67aa372e25d2838fc3d9b57f8e6f9bd38978
Jesús <jesus@MacBook-Air-de-Jesus.local>
2026-04-23 00:20:29 +0200

    add project structure

---
+++ backend/src/test/java/com/backgammon/BackgammonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backgammon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgammonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
